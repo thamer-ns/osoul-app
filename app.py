3dfc57a3ad422d5b16760f09c63a65650b5bd3c8
thamer-ns <110066889+thamer-ns@users.noreply.github.com>
2026-01-26 01:28:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from styles import apply_custom_css
 from security import login_system, logout
 from views import router
 
-# 1. إعداد الصفحة
 st.set_page_config(
     page_title=APP_NAME, 
     page_icon=APP_ICON, 
@@ -12,14 +11,11 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# 2. تطبيق التصميم
 apply_custom_css()
 
-# 3. الحالة
 if 'page' not in st.session_state:
     st.session_state.page = 'home'
 
-# 4. التوجيه الأمني
 if login_system():
     if st.session_state.page == 'logout':
         logout()
