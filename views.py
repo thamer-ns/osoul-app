6a979f814df800fed59a2ecf6768266e5d276855
thamer-ns <110066889+thamer-ns@users.noreply.github.com>
2026-02-02 02:42:04 +0300

    Update views.py

--- views.py
+++ views.py
@@ -3,9 +3,10 @@
 
 from ui.pages.dashboard import view_dashboard
 from ui.pages.portfolio import view_portfolio
+from ui.pages.sukuk import view_sukuk_portfolio
 
 from views_impl import (
-    view_sukuk_portfolio, view_analysis,
+    view_analysis,
     view_cash_log, view_backtester_ui, render_pulse_dashboard,
     view_add_trade, view_tools, view_settings
 )
