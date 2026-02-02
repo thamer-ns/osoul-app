d776005a64df9c19162e0374acfd9a5004231b03
thamer-ns <110066889+thamer-ns@users.noreply.github.com>
2026-02-02 03:32:42 +0300

    Update __init__.py

--- ui/pages/analysis/__init__.py
+++ ui/pages/analysis/__init__.py
@@ -1,2 +1,10 @@
 # ui/pages/analysis/__init__.py
+# analysis/__init__.py
+"""
+DEPRECATED: هذا الباكيج قديم.
+المصدر الرسمي الآن: ui.pages.analysis
+هذا الملف موجود فقط لتجنب كسر أي imports قديمة.
+"""
+
+from ui.pages.analysis.page import view_analysis  # noqa: F401
 
