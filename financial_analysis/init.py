24ffed5d0041c5afd91b64781429dc694bd8419c
thamer-ns <110066889+thamer-ns@users.noreply.github.com>
2026-02-09 16:39:15 +0300

    Delete financial_analysis directory

--- financial_analysis/init.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility shim.
-
-Some deployments or older code may reference `financial_analysis.init`.
-This file re-exports the public API from `financial_analysis` package.
-"""
-
-from .__init__ import *  # noqa: F401,F403
