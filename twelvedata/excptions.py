f62f01f76b0524276c1403ceab1fd35603f547cf
thamer-ns <110066889+thamer-ns@users.noreply.github.com>
2026-02-25 00:46:27 +0300

    Update excptions.py

--- twelvedata/excptions.py
+++ twelvedata/excptions.py
@@ -1,13 +1,16 @@
-# Compatibility exceptions module for vendored twelvedata package.
+# coding: utf-8
 
 class TwelveDataError(Exception):
-    pass
+    """Base exception for Twelvedata SDK compatibility."""
+
 
 class InvalidApiKeyError(TwelveDataError):
     pass
 
+
 class BadRequestError(TwelveDataError):
     pass
+
 
 class InternalServerError(TwelveDataError):
     pass
